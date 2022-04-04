1983260fbc33ec95a16cdcc9075c8a9e25073fe1
Ankit Sharma <83556797+Ankit-Sharma-97@users.noreply.github.com>
2022-04-04 17:00:42 +0530

    Create Count Set Bits.cpp
    use to count all ones

---
+++ Count Set Bits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n=13; 
+    int count=0;
+    for(int i=1;i<32;i=i*2){ /* 1=0001, 2=0010, 4=0100, 8=1000, etc */
+    if((n&i)!=0){  /*checking each bits of n with multiplying by 1,2,4,8,.... 2^32*/ 
+        count++;
+        }
+    
+    }
+    cout<<count;
+
+    return 0;
+}
